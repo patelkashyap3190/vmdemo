956a4d785e8a01e512ab7ffaeb1e7f5468c0c7d5
patelkashyap3190 <60091164+patelkashyap3190@users.noreply.github.com>
2020-01-20 14:55:25 +0530

    Add files via upload

---
+++ ReadFromCSVNestedPS.ps1
@@ -0,0 +1,14 @@
+﻿
+#Read from CSV
+Import-Csv D:\GithubProj\PowerShellWG\meetup\VM-Input2.csv 
+    ForEach-Object {       
+        
+        Write-Host $_.VMName, $_.RGName
+
+        #Call another PowerShell file (.ps1) with Parameters
+        $ScriptToRun = $PSScriptRoot+"\CreateVM-parameter.ps1" + " -VMName " + $_.VMName + " -RGName " + $_.RGName
+        
+        Invoke-Expression $ScriptToRun
+    }
+
+
